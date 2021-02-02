5dc73bd759b9784e2dc120eabf904439a17e98c5
erdi akpınar <erdiakpinar@doganhukuk.com>
2021-02-02 23:41:15 +0300

    AddingData&CreatingJsonServer

---
+++ ShopApp/src/data.js
@@ -0,0 +1,21 @@
+module.exports = function(){
+    return {
+        products: [
+            {id:1, name:"Iphone 6 plus", price:3000, imageUrl:"1.jpg", description:"iphone 6 plus Apple'ın ilk 6'inch telefonu"},
+            {id:2, name:"Iphone 7 plus", price:3000, imageUrl:"2.jpg", description:"iphone 7 plus Apple'ın ikinci 6'inch telefonu"},
+            {id:3, name:"Iphone 8 plus", price:3000, imageUrl:"3.jpg", description:"iphone 8 plus Apple'ın üçüncü 6'inch telefonu"},
+            {id:4, name:"Iphone X max", price:3000, imageUrl:"4.jpg", description:"iphone X max Apple'ın ilk çentikli telefonu"},
+            {id:5, name:"Iphone Xs max", price:3000, imageUrl:"5.jpg", description:"iphone Xs max Apple'ın ikinci çentikli telefonu"},
+            {id:6, name:"Iphone 11 pro", price:3000, imageUrl:"6.jpg", description:"iphone 11 pro Apple'ın üçüncü çentikli telefonu"},
+            {id:7, name:"Iphone 12 pro", price:3000, imageUrl:"7.jpg", description:"iphone 12 pro Apple'ın son telefonu"}
+        ],
+        categories: [
+            {id:1, name:"Telefon"},
+            {id:2, name:"Bilgisayar"},
+            {id:3, name:"Elektronik"}
+        ],
+        orders:[
+
+        ]
+    }
+}
